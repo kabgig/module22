ae78946e767cd7023a0d4f4d6cf597ffdd6ea972
Ansar Ismagilov <ansar89kab@bk.ru>
2023-05-21 19:31:56 +0700

    batch queries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,75 @@
 package main.java;
-
 import java.sql.*;
-
 public class Main {
     private final static String HOST = "localhost"; // сервер базы данных
     private final static String DATABASENAME = "testDB"; // имя базы
     private final static String USERNAME = "postgres"; // учетная запись пользователя
     private final static String PASSWORD = "2701"; // пароль
-
-    public static void main(String[] args) {
+    static Connection connection;
+    public static void main(String[] args) throws SQLException {
+        //Строка для соединения с бд
         String url = "jdbc:postgresql://" + HOST + "/" + DATABASENAME + "?user=" + USERNAME + "&password=" + PASSWORD;
-        try (Connection connection = DriverManager.getConnection(url, USERNAME, PASSWORD)) {
+        connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+        try {
+            connection.setAutoCommit(false);
             if (connection == null)
                 System.err.println("Нет соединения с БД!");
             else {
-                System.out.println("Соединение с БД установлено.");
-                String SQL = "UPDATE test set name='Nick' where ID=9;"; // Добавление столбца name
-                try (PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
-                    preparedStatement.executeUpdate();
-                }
+                System.out.println("Соединение с БД установлено корректно");
+            }
+            String SQL = "Insert into test(ID,\"user\") VALUES(?,?);";
+            //Запрос на получение всех данных
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
+                //Часть кода, в котором мы добавляем несколько запросов для одной отправки данных
+                preparedStatement.setInt(1, 10);
+                preparedStatement.setString(2, "Olaf");
+                preparedStatement.addBatch();
+
+                preparedStatement.setInt(1, 11);
+                preparedStatement.setString(2, "Erik");
+                preparedStatement.addBatch();
+
+                preparedStatement.setInt(1, 12);
+                preparedStatement.setString(2, "Baleog");
+                preparedStatement.addBatch();
+
+                int[] count = preparedStatement.executeBatch();
+                connection.commit();
+                System.out.println("Данные отправлены");
+            }
+        } catch (SQLException e) {
+            connection.rollback();
+            System.err.println("Данные не добавлены");
+            e.printStackTrace();
+        }
+    }
+    public static boolean checkvalue(int checkedvalue) {
+        String SQL = "Select * from test where ID=?;";
+        try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setInt(1, checkedvalue);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+    public static boolean insertvalue(int insertedvalue) {
+        String SQL = "insert  test(id) values(?)";
+        try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+            statement.setInt(1, insertedvalue);
+            int i = statement.executeUpdate();
+            if (i >= 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
         }
     }
 }
